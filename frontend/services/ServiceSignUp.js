de1b26d1277783510d908a2ba9c110179c9a6b5c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-08-06 16:43:51 +0530

    Comments updated in FE services file

--- frontend/services/ServiceSignUp.js
+++ frontend/services/ServiceSignUp.js
@@ -9,22 +9,30 @@ app.service('serviceSignUp', function ($http, $location) {
     console.log("baseUrl :: ", baseUrl);
 
     this.signUpUser = function (data, $scope) {
-        console.log("data on service register--- ", data);
+        console.log("data on service register --- ", data);
 
+        // Make an HTTP POST request to sign up a new user
         $http({
             method: 'POST',
             url: baseUrl + '/signup',
             data: data
         }).then(
+            // Success callback
             function successCallback(response) {
                 console.log("Register successful");
                 console.log(response);
+
+                // Update the message in the provided $scope
                 $scope.message = "Register successful";
 
+                // Redirect to the login page using $location
                 $location.path('/login');
             },
+            // Error callback
             function errorCallback(response) {
                 console.log("Register unsuccessful");
+
+                // Update the message in the provided $scope with the error message from the response
                 $scope.message = response.data.message.message;
             }
         );
--- frontend/services/chatServices.js
+++ frontend/services/chatServices.js
@@ -8,6 +8,7 @@ app.service('chatServices', function ($http) {
     var baseUrl = window.location.origin;
 
     this.getAllUser = function (usertoken) {
+        // Make an HTTP GET request to retrieve all users
         return $http({
             method: 'GET',
             url: baseUrl + '/getAllUser',
@@ -16,16 +17,23 @@ app.service('chatServices', function ($http) {
             }
         }).then(function (response) {
             console.log("getAllUser =>>>> All users data : ", response.data.result);
+
+            // Return the array of user data
             return response.data.result;
         }).catch(function (error) {
             console.log("Error occurred while getting users ::::::: ", error);
-            return response.data.result = false;
+
+            // Return false if an error occurs
+            return false;
         });
     };
+
 
     this.userMsg = function () {
         var arr = [];
         var usertoken = localStorage.getItem('token');
+
+        // Make an HTTP GET request to the specified URL with the user token in the headers
         return $http({
             method: 'GET',
             url: baseUrl + '/userMsg',
@@ -35,8 +43,8 @@ app.service('chatServices', function ($http) {
         }).then(function (response) {
             console.log("userMsg =>>>> Response data :::::: ", response);
 
+            // If there are no messages, return an empty array
             if (response.data.result.length === 0) {
-                console.log("userMsg =>>>> Response data  ss:::::: ", response.data.result);
                 return response.data.result = [];
             }
 
@@ -44,36 +52,49 @@ app.service('chatServices', function ($http) {
             for (let i = 0; i < response.data.result.length; i++) {
                 var a = response.data.result[i];
 
+                // Check if the current message involves the user
                 // if (
                 //     (localStorage.getItem('userid') == a.senderId && localStorage.getItem('ruserId') == a.receiverId) ||
                 //     (localStorage.getItem('userid') == a.receiverId && localStorage.getItem('ruserId') == a.senderId)
                 // ) {
-
                 // arr.push(a);
+                // }
+
+                // Check if the current message is not sent by the user and update lastAaaaValue
+                if (localStorage.getItem('userid') !== a._id) {
+                    lastAaaaValue = a;
+                }
             }
-            if (localStorage.getItem('userid') !== a._id) {
-                lastAaaaValue = a;
-            }
-            // }
+
+            // Check if rusername is null and update it
             if (localStorage.getItem('rusername') === null) {
-                console.log("localStorage rusername was empty")
+                console.log("localStorage rusername was empty");
                 localStorage.setItem('rusername', lastAaaaValue.receiverName);
             }
 
+            // Check if ruserId is null and update it
             if (localStorage.getItem('ruserId') === null) {
-                console.log("localStorage ruserId was empty")
+                console.log("localStorage ruserId was empty");
                 localStorage.setItem('ruserId', lastAaaaValue.receiverId);
             }
 
+            // Return the array of messages
             return response.data.result;
         }).catch(function (error) {
             console.log("Error occurred while getting messages ::::::: ", error);
+
+            // Return false if an error occurs
             return response.data.result = false;
         });
     };
 
+
+    // Typeahead Search for Users
     this.searchUser = function (searchText) {
+        // Retrieve the user token from local storage
         var usertoken = localStorage.getItem('token');
+
+        // Make an HTTP POST request to search for users
         return $http({
             method: 'POST',
             url: baseUrl + '/searchUser',
@@ -83,11 +104,16 @@ app.service('chatServices', function ($http) {
                 'Content-Type': 'application/json'
             }
         }).then(function (response) {
-            console.log("getAllUser =>>>> Response ::::::: ", response);
+            // Log the response from the server
+            console.log("searchUser =>>>> Response ::::::: ", response);
+
+            // Return the search results
             return response.data;
         }).catch(function (error) {
             console.log("Error occurred while searching users ::::::: ", error);
             return [];
         });
     };
+
+
 });
